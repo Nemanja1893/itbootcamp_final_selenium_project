0b00823c95b21d24f04200eadd3fd9821ba79113
Nemanja Nikolic <gcsejn@gmail.com>
2023-02-10 21:00:38 +0100

    Auth routes tests uploaded

--- src/test/java/Tests/AuthRoutesTests.java
+++ src/test/java/Tests/AuthRoutesTests.java
@@ -1,14 +1,33 @@
 package Tests;
 
 import com.sun.org.glassfish.gmbal.Description;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AuthRoutesTests extends BasicTest{
 
     @Test(priority = 10)
-    @Description("Test #1: Forbids visits to home url if not authenticated")
+    @Description("Forbids visits to home url if not authenticated")
     public void homePage(){
         driver.navigate().to(baseUrl + "/home");
-
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url does not contain login");
+    }
+    @Test(priority = 20)
+    @Description("Forbids visits to profile url if not authenticated")
+    public void profilePage(){
+        driver.navigate().to(baseUrl + "/profile");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url does not contain login");
+    }
+    @Test(priority = 30)
+    @Description("Forbids visits to admin cities url if not authenticated")
+    public void adminCitiesPage(){
+        driver.navigate().to(baseUrl + "/admin/cities");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url does not contain login");
+    }
+    @Test(priority = 40)
+    @Description("Forbids visits to admin users url if not authenticated")
+    public void adminUsersPage(){
+        driver.navigate().to(baseUrl + "/admin/users");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/login", "Url does not contain login");
     }
 }
